894fc6a980ff28e2ab1e1ab7133a5c107fe51224
Lukas Pazera <lukasp@wix.com>
2021-08-20 15:36:23 +0300

    shortened

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
-// require("readline")
-//     .createInterface({ input: process.stdin })
-//     .on("line", (line) => console.log(line + " => " + toBerlinClock(line)));
+require("readline")
+    .createInterface({ input: process.stdin })
+    .on("line", (line) => console.log(line + " => " + toBerlinClock(line)));
 
-const testCases = ["00:00:00", "00:00:01", "10:10:10", "22:23:18"];
+// const testCases = ["00:00:00", "00:00:01", "10:10:10", "22:23:18"];
 
-function getBerlinSeconds(seconds) {
-    return +seconds % 2 === 0 ? "." : "X";
-}
+const getBerlinSeconds = (seconds) => (+seconds % 2 === 0 ? "." : "X");
 
-function getBerlinString(time, dec) {
-    if (time < dec) return "";
-    return "X" + getBerlinString(time - dec, dec);
-}
+const getBerlinString = (time, dec) =>
+    time < dec ? "" : "X" + getBerlinString(time - dec, dec);
 
-function formatString(string, length) {
-    return string.padEnd(length, ".");
-}
+const formatString = (string, length) => string.padEnd(length, ".");
 
-function formatFiveMinuteString(string) {
-    return formatString(string.replace(/(..)./g, "$1|"), 11);
-}
+const formatFiveMinuteString = (string) =>
+    formatString(string.replace(/(..)./g, "$1|"), 11);
 
-function getBerlinHours(hours) {
+const getBerlinHours = (hours) => {
     const fiveHourString = formatString(getBerlinString(hours, 5), 4);
     const oneHourString = formatString(getBerlinString(hours % 5, 1), 4);
     return `${fiveHourString} ${oneHourString}`;
-}
+};
 
-function getBerlinMinutes(minutes) {
+const getBerlinMinutes = (minutes) => {
     const fiveMinuteString = formatFiveMinuteString(
         getBerlinString(minutes, 5)
     );
     const minuteString = formatString(getBerlinString(minutes % 5, 1), 4);
     return `${fiveMinuteString} ${minuteString}`;
-}
+};
 
-function toBerlinClock(line) {
+const toBerlinClock = (line) => {
     const [hours, minutes, seconds] = line.split(":");
     return `${getBerlinSeconds(seconds)} ${getBerlinHours(
         +hours
     )} ${getBerlinMinutes(+minutes)}`;
-}
+};
 
-function runTests() {
-    // console.log(getBerlinHours("10"));
-    testCases.forEach((time) =>
-        console.log(`${time} => ${toBerlinClock(time)}`)
-    );
-}
+// function runTests() {
+//     // console.log(getBerlinHours("10"));
+//     testCases.forEach((time) =>
+//         console.log(`${time} => ${toBerlinClock(time)}`)
+//     );
+// }
 
-runTests();
+// runTests();
